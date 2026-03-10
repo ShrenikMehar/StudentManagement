474f09a972ce21b5bbda6a7f1b00fc7db82c8904
ShrenikMehar <meharshrenik1@gmail.com>
2026-03-10 15:27:16 +0530

    Add info and debug loggers in controller

--- src/main/kotlin/org/one2n/controller/StudentController.kt
+++ src/main/kotlin/org/one2n/controller/StudentController.kt
@@ -4,6 +4,7 @@ import io.micronaut.http.annotation.*
 import org.one2n.dto.StudentRequest
 import org.one2n.dto.StudentResponse
 import org.one2n.service.StudentService
+import org.slf4j.LoggerFactory
 import java.util.UUID
 
 @Controller("/api/v1/students")
@@ -11,28 +12,51 @@ class StudentController(
     private val studentService: StudentService
 ) {
 
+    private val logger = LoggerFactory.getLogger(StudentController::class.java)
+
     @Get
     fun getAllStudents(): List<StudentResponse> {
-        return studentService.getAllStudents()
+        logger.debug("Received request to fetch all students")
+        val students = studentService.getAllStudents()
+
+        logger.debug("Returning {} students", students.size)
+        return students
     }
 
     @Post
     fun createStudent(@Body request: StudentRequest): StudentResponse {
-        return studentService.createStudent(request)
+        logger.debug("Received request to create student")
+        val student = studentService.createStudent(request)
+
+        logger.info("Student created successfully with id={}", student.id)
+        return student
     }
 
     @Get("/{id}")
     fun getStudentById(@PathVariable id: UUID): StudentResponse {
-        return studentService.getStudentById(id)
+        logger.debug("Received request to fetch student with id={}", id)
+        val student = studentService.getStudentById(id)
+
+        logger.debug("Successfully fetched student with id={}", id)
+        return student
     }
 
     @Put("/{id}")
     fun updateStudent(@PathVariable id: UUID, @Body request: StudentRequest): StudentResponse {
-        return studentService.updateStudent(id, request)
+        logger.debug("Received request to update student with id={}", id)
+
+        val updatedStudent = studentService.updateStudent(id, request)
+        logger.info("Student updated successfully with id={}", id)
+
+        return updatedStudent
     }
 
     @Delete("/{id}")
     fun deleteStudent(@PathVariable id: UUID): StudentResponse {
-        return studentService.deleteStudent(id)
+        logger.debug("Received request to delete student with id={}", id)
+        val deletedStudent = studentService.deleteStudent(id)
+
+        logger.info("Student deleted successfully with id={}", id)
+        return deletedStudent
     }
 }
