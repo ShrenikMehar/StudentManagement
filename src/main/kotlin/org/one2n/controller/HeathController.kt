32893c7aaeba7f08193ab1d8272bc264581d8dda
ShrenikMehar <meharshrenik1@gmail.com>
2026-03-10 12:25:10 +0530

    Add Health check endpoint

---
+++ src/main/kotlin/org/one2n/controller/HeathController.kt
@@ -0,0 +1,13 @@
+package com.shrenik.studentmanagement.controller
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller
+class HealthController {
+
+    @Get("/healthcheck")
+    fun health(): Map<String, String> {
+        return mapOf("status" to "ok")
+    }
+}
---
+++ src/test/kotlin/org/one2n/controller/HealthControllerTest.kt
@@ -0,0 +1,25 @@
+package org.one2n.controller
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.HttpClient
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class HealthControllerTest {
+
+    @Inject
+    @field:Client("/")
+    lateinit var client: HttpClient
+
+    @Test
+    fun `healthcheck endpoint returns ok`() {
+        val request = HttpRequest.GET<Any>("/healthcheck")
+        val response = client.toBlocking().retrieve(request)
+
+        assertEquals("""{"status":"ok"}""", response)
+    }
+}
